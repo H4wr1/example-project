9688f7c2b1cf007be7180bab755621161176263e
Mikołaj Hawryluk <hawrylukmikolaj@gmail.com>
2023-04-13 09:55:26 +0200

    Zadanie 1.10

--- solo-work/solo_1.py
+++ solo-work/solo_1.py
@@ -95,3 +95,43 @@ for student in studenci:
     if student.split()[-1].startswith('N'):
         liczba_n +=1
 print("Liczba studentow na N wynosi: {}".format(liczba_n))
+
+# zadanie 1.10
+
+# zmienne poniezej preprezentuja ulozenie punktow na wykresie,
+# do zadania dolaczono takze rysunek pomocniczy
+wykres_1 = [[2, 4], [4, 4], [6, 4]]
+wykres_2 = [[2, 3], [4, 4], [6, 5]]
+wykres_3 = [[2, 3], [4, 3], [5, 4]]
+
+# zbadaj kazdy wykres, czy dla wyznaczonych punktow istnieje funkcja
+# liniowa laczaca punkty
+# jesli sie nie da, to zwroc False
+# jesli sie da, zwroc True
+
+def czy_liniowa(punkty: list) :
+    return (punkty[0][1] - punkty[1][1]) * (punkty[0][0] - punkty[2][0]) == (punkty[0][1] - punkty[2][1]) * (punkty[0][0] - punkty[1][0])
+
+wykres_1_funkcja_liniowa = czy_liniowa(wykres_1)
+wykres_2_funkcja_liniowa = czy_liniowa(wykres_2)
+wykres_3_funkcja_liniowa = czy_liniowa(wykres_3)
+
+if wykres_1_funkcja_liniowa:
+    print("Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_1 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_2_funkcja_liniowa:
+    print("Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_2 nie mozna wyznaczyc funkcji liniowej.")
+
+if wykres_3_funkcja_liniowa:
+    print("Dla punktow w wykres_3 mozna wyznaczyc funkcje liniowa.")
+else:
+    print("Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.")
+
+# oczekiwany rezultat:
+# Dla punktow w wykres_1 mozna wyznaczyc funkcje liniowa.
+# Dla punktow w wykres_2 mozna wyznaczyc funkcje liniowa.
+# Dla punktow w wykres_3 nie mozna wyznaczyc funkcji liniowej.
