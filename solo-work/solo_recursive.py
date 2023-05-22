d485276e99e1c410015f5aab57cc5fcaf5b5a29b
Mikołaj Hawryluk <hawrylukmikolaj@gmail.com>
2023-05-22 12:53:52 +0200

    Poprawki_1

--- solo-work/solo_recursive.py
+++ solo-work/solo_recursive.py
@@ -1,7 +1,7 @@
 # fun sum l
 #   is empty?
-#       y->0
-#       n->l(0)+sum(reszta)
+#       y -> 0
+#       n -> l(0)+sum(reszta)
 def sum_of_list(lista):
     if len(lista)==0:
         return 0
@@ -11,8 +11,8 @@ print(sum_of_list([1, 3, 4, 2, 5]))
 
 # fun factorial n
 #   is 1?
-#       y->n
-#       n->n*factorial(n-1)
+#       y -> n
+#       n -> n*factorial(n-1)
 def factorial(n):
    if n == 1:
        return n
@@ -24,10 +24,14 @@ print(factorial(4))
 # fun find_max l
 #   is 1?
 #       y->l(0)
-#       n->maximumof(l(0),find_max(reszta))
+#       n -> is l(0)>find_max(reszta)?
+#               y->l(0)
+#               n-> l(reszta)  
 def find_max(lista):
     if len(lista) == 1:
         return lista[0]
     else:
-        return max(lista[0],find_max(lista[1:]))
+        return max(lista[0], find_max(lista[1:]))
+    
+
 print(find_max([1, 3, 4, 2, 5]))
